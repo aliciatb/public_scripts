849c702e6150096b4f4fe1485434b197a8a02e9f
Alicia <aliciatb@gmail.com>
2014-07-29 15:25:53 -0700

    Checking location for team if other

--- 12thman/parseTweets.py
+++ 12thman/parseTweets.py
@@ -4,7 +4,7 @@ import json
 import re
 
 aggie = ['a&m','aggie','aggiefootball','aggieland','aggielandticket','aggies','cfb','college station','college','em','gig','houston','hullabaloo','johnny','kyle','manziel','midnight','SECNetwork','whoop','yell']
-seahawk = ['12s','48','carroll','century','clink','dangeruss','gohawks','harbaugh','hawk','hawks','link','lob','money','moneylynch','nfc','nfl','pete','pnw','sb48','seagals','seahawk','seahawks','seattle','sherman','super','superbowl','vmac','west','wilson']
+seahawk = ['12s','48','beastmode','carroll','century','clink','dangeruss','gohawks','harbaugh','hawk','hawks','legionofboom','link','lob','money','moneylynch','nfc','nfl','pete','pnw','russ','sb48','seagals','seahawk','seahawks','seattle','sherman','super','superbowl','vmac','west','wilson']
 
 def getTeam(aggies, seahawks):
   """
@@ -17,12 +17,25 @@ def getTeam(aggies, seahawks):
   else:
     return "Other"  
 
+def getTeamByLocation(location):
+  """
+  assume that northwest folks prefer the Seahawks
+  """
+  loc = location.lower()
+  if re.search(r'washington|wa|seattle|north|west', loc):
+    return "Seahawk"
+  elif re.search(r'college|station|tx|texas|college station|houston', loc):
+    return "Aggie"
+  else:
+    return "Other"
+
 def scrubWord(word):
   """
   set all words to lowercase and reg expression to remove non alpha numeric
   """
   scrubbed = word.lower()
   scrubbed = re.sub(r'[^a-zA-Z0-9]','', scrubbed)
+  # todo: emoticons!
   return scrubbed
 
 def parseTweets(tweets):
@@ -40,14 +53,22 @@ def parseTweets(tweets):
         aggieTerms = 0
         seahawkTerms = 0
         for w in words:
-          # print w
           scrubbed_word = scrubWord(w)
           if scrubbed_word in aggie:
             aggieTerms += 1
           if scrubbed_word in seahawk:
             seahawkTerms += 1
+        team = getTeam(aggieTerms, seahawkTerms)
+        # location of tweeter
+        if tweet["user"]["location"] <> "":
+          location = tweet["user"]["location"]
+          if team == "Other":
+            team = getTeamByLocation(location)
+        if tweet["user"]["created_at"] <> "":
+          stamp = tweet["user"]["created_at"]
         # add the tweet and fan affiliation
-        fanTweet[tweet["text"]] = getTeam(aggieTerms, seahawkTerms)  
+        fanTweet[tweet["text"]] = team
+        
     except:
       exList.append(tweet)
   return fanTweet
@@ -57,6 +78,7 @@ def saveResults(results):
   output to csv for analysis
   """
   f = open("output.csv", "w")
+  f.write('team'+ "," + 'tweet' + "\n")
   for key in results.keys():
     f.write(results[key] + "," + key.encode('utf-8') + "\n")
     print results[key], key
